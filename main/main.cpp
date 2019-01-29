652253467ccc025b47b6d2fe09bdd75f2743a198
Samuel Munyili <samuel.munyili@andela.com>
2019-01-29 10:30:51 +0300

    pointers and references

--- main/main.cpp
+++ main/main.cpp
@@ -1,93 +1,29 @@
 #include <iostream>
 #include "pointers.cpp"
+#include "references.cpp"
 
 using namespace std;
 
 
-class GetStarted {
-
-public:
-    double getSum(double x, double y) {
-        return x + y;
-    }
-
-public:
-    bool isGreater(double x, double y) {
-        return x > y;
-    }
-
-public:
-    void loopAndCallName() {
-        for(int x= 0; x<= 10; x++){
-            cout << "Samuel, I have called you " << x << " times " << endl;
-        }
-    }
-
-public:
-    void whileLoopWithoutDo() {
-        long long x = 0;
-        printf("While Loop, Calling Samuel.....\n");
-        while (x < 0){
-            cout << "Samuel, I have called you " << x << " times " << endl;
-            x ++;
-        }
-    }
-
-public:
-    void whileLoopWithDo() {
-        long long x = 0;
-        printf("Do While Loop, Calling Samuel.....\n");
-        do{
-            cout << "Samuel, I have called you " << x << " times " << endl;
-            x ++;
-        }
-        while (x < 0);
-    }
-
-};
-
-struct list_emelemt {
-    struct list_element * next;
-};
-
-typedef struct list_element list_element;
-
-typedef struct {
-    list_element * first;
-    list_element * last;
-} list;
-
-
-
-
 int main() {
-    printf("Hello World!! I'm Samuel\n");
-
-    int count = 2;
-    int *pointer = &count;
-
-    cout << "The pointer is " << *pointer << endl;
-
-    GetStarted getStarted{};
-
-    cout << "The sum is " << getStarted.getSum(20, 40.89808999) << endl;
-
-    cout<<"Is greater ? " <<  getStarted.isGreater(100, 10.1) << endl;
-
-    long myLong[] = {10, 15, 20};
-
-    cout <<"My long is " << myLong[4] << endl;
-
-    printf("Calling Samuel ........\n");
-    getStarted.loopAndCallName();
-
-    getStarted.whileLoopWithoutDo();
-
-    getStarted.whileLoopWithDo();
 
     Pointers pointers{};
 
     pointers.showName();
+
+    pointers.showDifference();
+
+    pointers.handleReferences();
+
+
+    cout << "\n\n\n\n" << endl;
+    References references{};
+    int initialNum = 10;
+    references.changeNumber(initialNum);
+    cout << "In the main, the initial number is now " << initialNum << endl;
+
+    references.changeNumberRef(initialNum);
+    cout << "In the main, the initial number after referencing is now " << initialNum << endl;
 
     return 0;
 }
--- main/pointers.cpp
+++ main/pointers.cpp
@@ -10,7 +10,62 @@ using namespace std;
 class Pointers {
 
 public:
-    void showName(){
+    void showName() {
         printf("My name is still Samuel Munyii.\n");
     }
+
+public:
+    int num = 10;
+    int *pNum = &num;
+
+public:
+    void showDifference() {
+        cout << "The number " << num << " is not equal to " << pNum
+             << " but they are actually equal. Lol" << " although, they look the same as the dereference version "
+             << *pNum << endl;
+
+        // to make it more interesting, let us reassign the memory
+
+        int x = 20;
+        int *y = &x;
+
+        // print to confirm
+        cout << " x is " << x << " and y is " << y << endl;
+
+        // now declare z and assign the value of x using the dereference
+        int z = *y;
+
+        cout << "The value os z is " << z << endl;
+
+//        finally, reassign x using its pointer, let see if it changes
+        *y = 30;
+        cout << "Did it change? x is " << x << " and y is " << y << " and z is " << z << endl;
+
+    }
+
+public:
+    bool handleReferences() {
+        printf("references ---------------------------------\n");
+
+        int x = 20;
+        int &y = x;
+
+        // print to confirm
+        cout << " x is " << x << " and y is " << y << endl;
+
+        // now let us change the value of x and see if the value oy automatically changes
+        x = 100;
+        cout << "After the change:\n x is " << x << " and y is " << y << endl;
+
+        // what is we change y instead, will x change? Let's give it a shot
+
+        y = 200;
+
+        cout << "After the changing y, will x change?:\n x is " << x << " and y is " << y << endl;
+////        cout << "Did it change? x is " << x << " and y is " << y << " and z is " << z << endl;
+
+        return false;
+    }
+
+
 };
